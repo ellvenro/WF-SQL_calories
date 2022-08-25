e2c232e5173f508762950821151d25ed9d5082c0
ellvenro <lidiya.ellie@yandex.ru>
2022-08-25 17:55:36 +0300

    Связь с БД, добавление списка приемов пищи

--- WF-MSA_calories/Form1.Designer.cs
+++ WF-MSA_calories/Form1.Designer.cs
@@ -124,6 +124,7 @@
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
--- WF-MSA_calories/Form1.cs
+++ WF-MSA_calories/Form1.cs
@@ -7,14 +7,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.OleDb;
 
 namespace WF_MSA_calories
 {
     public partial class Form1 : Form
     {
+        public static string connectString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=Database.mdb;";
+        private OleDbConnection myConnection;
         public Form1()
         {
             InitializeComponent();
+            myConnection = new OleDbConnection(connectString);
+            myConnection.Open();
+            //Заполнение списка приемов пищи
+            string query = "SELECT eating.e_meal FROM eating";
+            OleDbCommand command = new OleDbCommand(query, myConnection);
+            OleDbDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                comboBox1.Items.Add(reader[0].ToString());
+            }
+            reader.Close();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            myConnection.Close();
         }
     }
 }
