8d21334f56bbc41b219175e86c3c921973f776b3
ellvenro <lidiya.ellie@yandex.ru>
2022-08-25 18:08:57 +0300

    Добавление списка в таблицу

--- WF-MSA_calories/Form1.cs
+++ WF-MSA_calories/Form1.cs
@@ -13,13 +13,25 @@ namespace WF_MSA_calories
 {
     public partial class Form1 : Form
     {
+        /// <summary>
+        /// Строка подключения к MS Access
+        /// </summary>
         public static string connectString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=Database.mdb;";
+
+        /// <summary>
+        /// Поле - ссылка на экземпляр класса OleDbConnection для соединения с БД
+        /// </summary>
         private OleDbConnection myConnection;
+
+        /// <summary>
+        /// Инициализация формы
+        /// </summary>
         public Form1()
         {
             InitializeComponent();
             myConnection = new OleDbConnection(connectString);
             myConnection.Open();
+
             //Заполнение списка приемов пищи
             string query = "SELECT eating.e_meal FROM eating";
             OleDbCommand command = new OleDbCommand(query, myConnection);
@@ -29,11 +41,60 @@ namespace WF_MSA_calories
                 comboBox1.Items.Add(reader[0].ToString());
             }
             reader.Close();
+
+            //Заполнение списка категорий в таблице
+            query = "SELECT categoryes.c_category FROM categoryes";
+            command = new OleDbCommand(query, myConnection);
+            reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                Column1.Items.Add(reader[0].ToString());
+            }
+            reader.Close();
         }
 
+        /// <summary>
+        /// Закрытие формы
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
             myConnection.Close();
+        }
+
+        /// <summary>
+        /// смена приема пищи
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            String index = comboBox1.SelectedValue.ToString();
+            if (index == "Завтрак")
+            {
+
+            }
+            else if (index == "Второй завтрак")
+            {
+
+            }
+            else if (index == "Обед")
+            {
+
+            }
+            else if (index == "Полдник")
+            {
+
+            }
+            else if (index == "Ужин")
+            {
+
+            }
+            else
+            {
+
+            }
         }
     }
 }
