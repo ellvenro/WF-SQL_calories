fa4c46cf96b44986a863bf1b7bb9c2b8a18cbf96
ellvenro <lidiya.ellie@yandex.ru>
2022-08-28 20:51:16 +0300

    Отчет за день

--- WF-MSA_calories/reportForm.Designer.cs
+++ WF-MSA_calories/reportForm.Designer.cs
@@ -28,19 +28,356 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listView3 = new System.Windows.Forms.ListView();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listView4 = new System.Windows.Forms.ListView();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.listView5 = new System.Windows.Forms.ListView();
+            this.groupBox6 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.listView6 = new System.Windows.Forms.ListView();
+            this.label7 = new System.Windows.Forms.Label();
+            this.listView7 = new System.Windows.Forms.ListView();
+            this.listView8 = new System.Windows.Forms.ListView();
+            this.listView9 = new System.Windows.Forms.ListView();
+            this.listView10 = new System.Windows.Forms.ListView();
+            this.listView11 = new System.Windows.Forms.ListView();
+            this.listView12 = new System.Windows.Forms.ListView();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.groupBox6.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listView7);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.listView1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(260, 221);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Завтрак";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(193, 201);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // listView1
+            // 
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(6, 21);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(169, 177);
+            this.listView1.TabIndex = 1;
+            this.listView1.TileSize = new System.Drawing.Size(228, 80);
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.listView8);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.listView2);
+            this.groupBox2.Location = new System.Drawing.Point(12, 239);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(260, 221);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Второй завтрак";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(193, 201);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // listView2
+            // 
+            this.listView2.HideSelection = false;
+            this.listView2.Location = new System.Drawing.Point(6, 21);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(169, 177);
+            this.listView2.TabIndex = 1;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.List;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.listView9);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.listView3);
+            this.groupBox3.Location = new System.Drawing.Point(278, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(260, 301);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Обед";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(193, 281);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "label3";
+            // 
+            // listView3
+            // 
+            this.listView3.HideSelection = false;
+            this.listView3.Location = new System.Drawing.Point(6, 21);
+            this.listView3.Name = "listView3";
+            this.listView3.Size = new System.Drawing.Size(169, 257);
+            this.listView3.TabIndex = 1;
+            this.listView3.UseCompatibleStateImageBehavior = false;
+            this.listView3.View = System.Windows.Forms.View.List;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.listView10);
+            this.groupBox4.Controls.Add(this.label4);
+            this.groupBox4.Controls.Add(this.listView4);
+            this.groupBox4.Location = new System.Drawing.Point(278, 319);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(260, 138);
+            this.groupBox4.TabIndex = 3;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Полдник";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(193, 118);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 17);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "label4";
+            // 
+            // listView4
+            // 
+            this.listView4.HideSelection = false;
+            this.listView4.Location = new System.Drawing.Point(6, 22);
+            this.listView4.Name = "listView4";
+            this.listView4.Size = new System.Drawing.Size(169, 93);
+            this.listView4.TabIndex = 1;
+            this.listView4.UseCompatibleStateImageBehavior = false;
+            this.listView4.View = System.Windows.Forms.View.List;
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.listView11);
+            this.groupBox5.Controls.Add(this.label5);
+            this.groupBox5.Controls.Add(this.listView5);
+            this.groupBox5.Location = new System.Drawing.Point(544, 12);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(260, 250);
+            this.groupBox5.TabIndex = 4;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Ужин";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(193, 227);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 17);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "label5";
+            // 
+            // listView5
+            // 
+            this.listView5.HideSelection = false;
+            this.listView5.Location = new System.Drawing.Point(6, 21);
+            this.listView5.Name = "listView5";
+            this.listView5.Size = new System.Drawing.Size(169, 200);
+            this.listView5.TabIndex = 1;
+            this.listView5.UseCompatibleStateImageBehavior = false;
+            this.listView5.View = System.Windows.Forms.View.List;
+            // 
+            // groupBox6
+            // 
+            this.groupBox6.Controls.Add(this.listView12);
+            this.groupBox6.Controls.Add(this.label6);
+            this.groupBox6.Controls.Add(this.listView6);
+            this.groupBox6.Location = new System.Drawing.Point(544, 268);
+            this.groupBox6.Name = "groupBox6";
+            this.groupBox6.Size = new System.Drawing.Size(260, 166);
+            this.groupBox6.TabIndex = 4;
+            this.groupBox6.TabStop = false;
+            this.groupBox6.Text = "Перед сном";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(193, 146);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(46, 17);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "label6";
+            // 
+            // listView6
+            // 
+            this.listView6.HideSelection = false;
+            this.listView6.Location = new System.Drawing.Point(6, 22);
+            this.listView6.Name = "listView6";
+            this.listView6.Size = new System.Drawing.Size(169, 121);
+            this.listView6.TabIndex = 1;
+            this.listView6.UseCompatibleStateImageBehavior = false;
+            this.listView6.View = System.Windows.Forms.View.List;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(737, 440);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(46, 17);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "label7";
+            // 
+            // listView7
+            // 
+            this.listView7.Cursor = System.Windows.Forms.Cursors.Default;
+            this.listView7.GridLines = true;
+            this.listView7.HideSelection = false;
+            this.listView7.Location = new System.Drawing.Point(181, 21);
+            this.listView7.Name = "listView7";
+            this.listView7.Size = new System.Drawing.Size(73, 177);
+            this.listView7.TabIndex = 2;
+            this.listView7.TileSize = new System.Drawing.Size(228, 80);
+            this.listView7.UseCompatibleStateImageBehavior = false;
+            this.listView7.View = System.Windows.Forms.View.List;
+            // 
+            // listView8
+            // 
+            this.listView8.HideSelection = false;
+            this.listView8.Location = new System.Drawing.Point(181, 21);
+            this.listView8.Name = "listView8";
+            this.listView8.Size = new System.Drawing.Size(73, 177);
+            this.listView8.TabIndex = 2;
+            this.listView8.UseCompatibleStateImageBehavior = false;
+            this.listView8.View = System.Windows.Forms.View.List;
+            // 
+            // listView9
+            // 
+            this.listView9.HideSelection = false;
+            this.listView9.Location = new System.Drawing.Point(181, 21);
+            this.listView9.Name = "listView9";
+            this.listView9.Size = new System.Drawing.Size(73, 257);
+            this.listView9.TabIndex = 3;
+            this.listView9.UseCompatibleStateImageBehavior = false;
+            this.listView9.View = System.Windows.Forms.View.List;
+            // 
+            // listView10
+            // 
+            this.listView10.HideSelection = false;
+            this.listView10.Location = new System.Drawing.Point(181, 22);
+            this.listView10.Name = "listView10";
+            this.listView10.Size = new System.Drawing.Size(73, 93);
+            this.listView10.TabIndex = 5;
+            this.listView10.UseCompatibleStateImageBehavior = false;
+            this.listView10.View = System.Windows.Forms.View.List;
+            // 
+            // listView11
+            // 
+            this.listView11.HideSelection = false;
+            this.listView11.Location = new System.Drawing.Point(181, 21);
+            this.listView11.Name = "listView11";
+            this.listView11.Size = new System.Drawing.Size(73, 200);
+            this.listView11.TabIndex = 5;
+            this.listView11.UseCompatibleStateImageBehavior = false;
+            this.listView11.View = System.Windows.Forms.View.List;
+            // 
+            // listView12
+            // 
+            this.listView12.HideSelection = false;
+            this.listView12.Location = new System.Drawing.Point(181, 22);
+            this.listView12.Name = "listView12";
+            this.listView12.Size = new System.Drawing.Size(73, 121);
+            this.listView12.TabIndex = 5;
+            this.listView12.UseCompatibleStateImageBehavior = false;
+            this.listView12.View = System.Windows.Forms.View.List;
             // 
             // reportForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(815, 471);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.groupBox6);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Name = "reportForm";
             this.Text = "reportForm";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox5.PerformLayout();
+            this.groupBox6.ResumeLayout(false);
+            this.groupBox6.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListView listView3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListView listView4;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListView listView5;
+        private System.Windows.Forms.GroupBox groupBox6;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ListView listView6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ListView listView7;
+        private System.Windows.Forms.ListView listView8;
+        private System.Windows.Forms.ListView listView9;
+        private System.Windows.Forms.ListView listView10;
+        private System.Windows.Forms.ListView listView11;
+        private System.Windows.Forms.ListView listView12;
     }
 }
--- WF-MSA_calories/reportForm.cs
+++ WF-MSA_calories/reportForm.cs
@@ -7,14 +7,80 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.OleDb;
 
 namespace WF_MSA_calories
 {
     public partial class reportForm : Form
-    {
-        public reportForm()
+    { 
+        /// <summary>
+        /// Поле - ссылка на экземпляр класса OleDbConnection для соединения с БД
+        /// </summary>
+        public OleDbConnection myConnection;
+
+        public List<GroupBox> listGB = new List<GroupBox>();
+        public List<ListView> listLV = new List<ListView>();
+        public List<ListView> listLV2 = new List<ListView>();
+        public List<Label> listL = new List<Label>();
+
+        public reportForm(OleDbConnection Connection)
         {
             InitializeComponent();
+            myConnection = Connection;
+
+            listGB.Add(groupBox1); listLV.Add(listView1); listL.Add(label1); listLV2.Add(listView7);
+            listGB.Add(groupBox2); listLV.Add(listView2); listL.Add(label2); listLV2.Add(listView8);
+            listGB.Add(groupBox3); listLV.Add(listView3); listL.Add(label3); listLV2.Add(listView9);
+            listGB.Add(groupBox4); listLV.Add(listView4); listL.Add(label4); listLV2.Add(listView10);
+            listGB.Add(groupBox5); listLV.Add(listView5); listL.Add(label5); listLV2.Add(listView11);
+            listGB.Add(groupBox6); listLV.Add(listView6); listL.Add(label6); listLV2.Add(listView12);
+
+            for (int i = 0; i < 6; i++)
+            {
+                listGB[i].Visible = false;
+                listLV[i].Visible = false;
+                listL[i].Visible = false;
+                listLV2[i].Visible = false;
+            }
+
+            string query = "SELECT eating.e_meal, eating.e_ccal FROM eating";
+            OleDbCommand command = new OleDbCommand(query, myConnection);
+            OleDbDataReader reader = command.ExecuteReader();
+            int sum = 0;
+            while (reader.Read())
+            {
+                string query1 = $"SELECT day.d_name, day.d_gramm, day.d_ccal FROM [day] WHERE day.d_meal=\"{reader[0].ToString()}\"";
+                OleDbCommand command1 = new OleDbCommand(query1, myConnection);
+                OleDbDataReader reader1 = command1.ExecuteReader();
+
+                for (int i = 0; i < 6; i++)
+                    if (reader[0].ToString() == listGB[i].Text)
+                    {
+                        int cnt = 0;
+                        while (reader1.Read())
+                        {
+                            if (reader1[0].ToString() != "")
+                            {
+                                listLV[i].Items.Add(reader1[0].ToString());
+                                listLV2[i].Items.Add(reader1[1].ToString() + "/" + reader1[2].ToString());
+                                cnt++;
+                            }
+                        }
+                        if (cnt != 0)
+                        {
+                            listL[i].Text = reader[1].ToString();
+                            sum += int.Parse(reader[1].ToString());
+                            listGB[i].Visible = true;
+                            listLV[i].Visible = true;
+                            listL[i].Visible = true;
+                            listLV2[i].Visible = true;
+                        }
+                    }
+                reader1.Close();
+
+            }
+            reader.Close();
+            label7.Text = sum.ToString();
         }
     }
 }
