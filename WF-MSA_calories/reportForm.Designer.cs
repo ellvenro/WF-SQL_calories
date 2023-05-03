a3a394d72594fb48b144b9e940b3ec6332e1772e
ellvenro <lidiya.ellie@yandex.ru>
2023-05-03 17:53:22 +0300

    Корректировка свойств отчета

--- WF-MSA_calories/reportForm.Designer.cs
+++ WF-MSA_calories/reportForm.Designer.cs
@@ -621,6 +621,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(1489, 569);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.dateTimePicker1);
